36d144037087818c72880165e81ba0be7150177a
Abdur Rahim <rahimbd623@gmail.com>
2019-12-01 16:33:24 +0600

    update database

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'elecolab.com',
 	'username' => 'elecolab_isp',
 	'password' => 'R.rahim623',
 	'database' => 'elecolab_isp',
